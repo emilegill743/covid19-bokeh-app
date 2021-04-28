ad70ee58884e82a7567cdb62a7b76e26b9d42c80
emilegill743 <58830015+emilegill743@users.noreply.github.com>
2021-04-28 06:54:21 +0100

    Updating etl.py to fix government covid-API call

--- app/data/etl.py
+++ app/data/etl.py
@@ -30,7 +30,7 @@ def etl_decorator(func):
 
 
 @etl_decorator
-def jhu_cases_etl():
+def jhu_cases_etl(db_engine):
 
     """ETL job for Johns Hopkins Global Cases Data"""
 
@@ -52,9 +52,9 @@ def jhu_cases_etl():
         jhu_df = pd.melt(
                     frame=jhu_df,
                     id_vars=['Country/Region',
-                            'Province/State',
-                            'Lat',
-                            'Long'],
+                             'Province/State',
+                             'Lat',
+                             'Long'],
                     var_name='Date',
                     value_name='cases')
 
@@ -65,36 +65,33 @@ def jhu_cases_etl():
             inplace=True)
 
         jhu_df = jhu_df.rename(columns={
-                    'Country/Region' : 'region',
-                    'Province/State' : 'province',
-                    'Lat' : 'lat',
-                    'Long' : 'long',
-                    'Date' : 'date',
+                    'Country/Region': 'region',
+                    'Province/State': 'province',
+                    'Lat': 'lat',
+                    'Long': 'long',
+                    'Date': 'date',
                     })
 
         return(jhu_df)
 
-    def load_data(jhu_df):
+    def load_data(jhu_df, db_engine):
 
-            """Load JHU data into Postgres Database"""
+        """Load JHU data into Postgres Database"""
 
-            connection_uri = os.environ['connection_uri']
-            db_engine = create_engine(connection_uri)
+        jhu_df.to_sql(
+            name="jhu_global_cases",
+            con=db_engine,
+            if_exists="replace",
+            index=False,
+            method='multi')
 
-            jhu_df.to_sql(
-                name="jhu_global_cases",
-                con=db_engine,
-                if_exists="replace",
-                index=False,
-                method='multi')
-    
     data = extract_data()
     data = transform_data(data)
-    load_data(data)
+    load_data(data, db_engine)
 
 
 @etl_decorator
-def jhu_deaths_etl():
+def jhu_deaths_etl(db_engine):
 
     """ETL job for Johns Hopkins Global Deaths Data"""
 
@@ -129,36 +126,33 @@ def jhu_deaths_etl():
             inplace=True)
 
         jhu_df = jhu_df.rename(columns={
-                    'Country/Region' : 'region',
-                    'Province/State' : 'province',
-                    'Lat' : 'lat',
-                    'Long' : 'long',
-                    'Date' : 'date',
+                    'Country/Region': 'region',
+                    'Province/State': 'province',
+                    'Lat': 'lat',
+                    'Long': 'long',
+                    'Date': 'date',
                     })
 
         return(jhu_df)
 
-    def load_data(jhu_df):
+    def load_data(jhu_df, db_engine):
 
-            """Load JHU data into Postgres Database"""
+        """Load JHU data into Postgres Database"""
 
-            connection_uri = os.environ['connection_uri']
-            db_engine = create_engine(connection_uri)
-
-            jhu_df.to_sql(
+        jhu_df.to_sql(
                 name="jhu_global_deaths",
                 con=db_engine,
                 if_exists="replace",
                 index=False,
                 method='multi')
-    
+
     data = extract_data()
     data = transform_data(data)
-    load_data(data)
+    load_data(data, db_engine)
 
 
 @etl_decorator
-def jhu_lookup_etl():
+def jhu_lookup_etl(db_engine):
 
     """ETL job for Johns Hopkins Reference 'Lookup' Table"""
 
@@ -173,17 +167,15 @@ def jhu_lookup_etl():
             continent_code = country_alpha2_to_continent_code(country_alpha2)
         except:
             continent_code = "N/A"
-        
+
         return(continent_code)
 
-    lookup_df['Continent'] = lookup_df['iso2'].apply(country_alpha2_to_continent)
+    lookup_df['Continent'] = lookup_df['iso2'].apply(
+                                country_alpha2_to_continent)
 
     lookup_df.rename(
-        columns={col : col.lower() for col in lookup_df.columns},
+        columns={col: col.lower() for col in lookup_df.columns},
         inplace=True)
-
-    connection_uri = os.environ['connection_uri']
-    db_engine = create_engine(connection_uri)
 
     lookup_df.to_sql(
         name="jhu_lookup",
@@ -194,7 +186,7 @@ def jhu_lookup_etl():
 
 
 @etl_decorator
-def jhu_us_cases_etl():
+def jhu_us_cases_etl(db_engine):
 
     """ETL job for Johns Hopkins US Deaths Data"""
 
@@ -215,7 +207,7 @@ def jhu_us_cases_etl():
                             columns=[
                                 'UID', 'iso2', 'iso3',
                                 'code3', 'FIPS', 'Admin2',
-                                'Combined_Key', 'Lat', 'Long_']) 
+                                'Combined_Key', 'Lat', 'Long_'])
         jhu_df = pd.melt(
                         frame=jhu_df,
                         id_vars=['Country_Region',
@@ -231,34 +223,34 @@ def jhu_us_cases_etl():
         jhu_df['Date'] = pd.to_datetime(jhu_df['Date'])
 
         jhu_df = jhu_df.rename(columns={
-                    'Country_Region' : 'region',
-                    'Province_State' : 'province',
-                    'Date' : 'date',
+                    'Country_Region': 'region',
+                    'Province_State': 'province',
+                    'Date': 'date',
                     })
 
         return(jhu_df)
 
-    def load_data(jhu_df):
+    def load_data(jhu_df, db_engine):
 
-            """Load JHU data into Postgres Database"""
+        """Load JHU data into Postgres Database"""
 
-            connection_uri = os.environ['connection_uri']
-            db_engine = create_engine(connection_uri)
+        connection_uri = os.environ['connection_uri']
+        db_engine = create_engine(connection_uri)
 
-            jhu_df.to_sql(
-                name="jhu_us_cases",
-                con=db_engine,
-                if_exists="replace",
-                index=False,
-                method='multi')
-    
+        jhu_df.to_sql(
+            name="jhu_us_cases",
+            con=db_engine,
+            if_exists="replace",
+            index=False,
+            method='multi')
+
     data = extract_data()
     data = transform_data(data)
-    load_data(data)
+    load_data(data, db_engine)
 
 
 @etl_decorator
-def jhu_us_deaths_etl():
+def jhu_us_deaths_etl(db_engine):
 
     """ETL job for Johns Hopkins US Deaths Data"""
 
@@ -296,45 +288,44 @@ def jhu_us_deaths_etl():
         jhu_df['Date'] = pd.to_datetime(jhu_df['Date'])
 
         jhu_df = jhu_df.rename(columns={
-                    'Country_Region' : 'region',
-                    'Province_State' : 'province',
-                    'Date' : 'date',
+                    'Country_Region': 'region',
+                    'Province_State': 'province',
+                    'Date': 'date',
                     })
 
         return(jhu_df)
 
-    def load_data(jhu_df):
+    def load_data(jhu_df, db_engine):
 
-            """Load JHU data into Postgres Database"""
+        """Load JHU data into Postgres Database"""
 
-            connection_uri = os.environ['connection_uri']
-            db_engine = create_engine(connection_uri)
+        connection_uri = os.environ['connection_uri']
+        db_engine = create_engine(connection_uri)
 
-            jhu_df.to_sql(
-                name="jhu_us_deaths",
-                con=db_engine,
-                if_exists="replace",
-                index=False,
-                method='multi')
-    
+        jhu_df.to_sql(
+            name="jhu_us_deaths",
+            con=db_engine,
+            if_exists="replace",
+            index=False,
+            method='multi')
+
     data = extract_data()
     data = transform_data(data)
-    load_data(data)
+    load_data(data, db_engine)
 
 
 @etl_decorator
-def us_states_etl():
+def us_states_etl(db_engine):
 
     """ETL job for loading US states
     Longitude/Latitude data"""
 
     dirname = os.path.dirname(__file__)
-    us_states_dataset_path = os.path.join(dirname, 'geo_data', 'kaggle_states.csv')
+    us_states_dataset_path = os.path.join(dirname,
+                                          'geo_data',
+                                          'kaggle_states.csv')
 
     us_states_df = pd.read_csv(us_states_dataset_path)
-
-    connection_uri = os.environ['connection_uri']
-    db_engine = create_engine(connection_uri)
 
     us_states_df.to_sql(
                 name="us_states_coords",
@@ -345,7 +336,7 @@ def us_states_etl():
 
 
 @etl_decorator
-def local_uk_data_etl():
+def local_uk_data_etl(db_engine):
 
     """ETL job for UK local data"""
 
@@ -353,90 +344,46 @@ def local_uk_data_etl():
 
         """Pull local UK data from gov.uk API"""
 
-        all_nations = [
+        endpoint = "https://api.coronavirus.data.gov.uk/v1/data"
+
+        filters = [
             "areaType=ltla"
         ]
 
-        cases_and_deaths = {
+        structure = {
             "date": "date",
             "areaName": "areaName",
             "areaCode": "areaCode",
             "newCasesByPublishDate": "newCasesByPublishDate",
             "cumCasesByPublishDate": "cumCasesByPublishDate"}
-            # "maleCases" : "maleCases",
-            # "femaleCases" : "femaleCases",
-            # "newPillarOneTestsByPublishDate" : "newPillarOneTestsByPublishDate",
-            # "cumPillarOneTestsByPublishDate" : "cumPillarOneTestsByPublishDate",
-            # "newPillarTwoTestsByPublishDate" : "newPillarTwoTestsByPublishDate",
-            # "cumPillarTwoTestsByPublishDate" : "cumPillarTwoTestsByPublishDate",
-            # "newPillarThreeTestsByPublishDate" : "newPillarThreeTestsByPublishDate",
-            # "cumPillarThreeTestsByPublishDate" : "cumPillarThreeTestsByPublishDate",
-            # "newPillarFourTestsByPublishDate" : "newPillarFourTestsByPublishDate",
-            # "cumPillarFourTestsByPublishDate" : "cumPillarFourTestsByPublishDate",
-            # "newAdmissions" : "newAdmissions",
-            # "cumAdmissions" : "cumAdmissions",
-            # "cumAdmissionsByAge" : "cumAdmissionsByAge",
-            # "cumTestsByPublishDate" : "cumTestsByPublishDate",
-            # "newTestsByPublishDate" : "newTestsByPublishDate",
-            # "covidOccupiedMVBeds" : "covidOccupiedMVBeds",
-            # "hospitalCases" : "hospitalCases",
-            # "plannedCapacityByPublishDate" : "plannedCapacityByPublishDate",
-            # "newDeaths28DaysByPublishDate" : "newDeaths28DaysByPublishDate",
-            # "cumDeaths28DaysByPublishDate" : "cumDeaths28DaysByPublishDate",
-            # "cumDeaths28DaysByPublishDateRate" : "cumDeaths28DaysByPublishDateRate",
-            # "newDeaths28DaysByDeathDate" : "newDeaths28DaysByDeathDate",
-            # "cumDeaths28DaysByDeathDate" : "cumDeaths28DaysByDeathDate",
-            # "cumDeaths28DaysByDeathDateRate" : "cumDeaths28DaysByDeathDateRate"}
 
-        api = Cov19API(
-            filters=all_nations,
-            structure=cases_and_deaths
-        )
+        api_params = {
+            "filters": str.join(";", filters),
+            "structure": json.dumps(structure, separators=(",", ":"))}
 
-        df = api.get_dataframe()
+        response = requests.get(endpoint, params=api_params, timeout=240)
+
+        if response.status_code >= 400:
+            raise RuntimeError(f'Request failed: { response.text }')
+
+        json_data = response.json()
+        df = pd.DataFrame(json_data["data"])
 
         return(df)
 
     def transform_data(local_uk_df):
 
         local_uk_df = local_uk_df.rename(columns={
-                            'areaName' : 'area_name',
-                            'areaCode' : 'area_code',
-                            'newCasesByPublishDate' : 'new_cases',
-                            'cumCasesByPublishDate' : 'cum_cases'})
-                            # 'maleCases' : 'male_cases',
-                            # 'femaleCases' : 'female_cases',
-                            # 'newPillarOneTestsByPublishDate' : 'new_pillar_one_tests',
-                            # 'cumPillarOneTestsByPublishDate' : 'cum_pillar_one_tests',
-                            # 'newPillarTwoTestsByPublishDate' : 'new_pillar_two_tests',
-                            # 'cumPillarTwoTestsByPublishDate' : 'cum_pillar_two_tests',
-                            # 'newPillarThreeTestsByPublishDate' : 'new_pillar_three_tests',
-                            # 'cumPillarThreeTestsByPublishDate' : 'cum_pillar_three_tests',
-                            # 'newPillarFourTestsByPublishDate' : 'new_pillar_four_tests',
-                            # 'cumPillarFourTestsByPublishDate' : 'cum_pillar_four_tests',
-                            # 'newAdmissions' : 'new_admissions',
-                            # 'cumAdmissions' : 'cum_admissions',
-                            # 'cumAdmissionsByAge' : 'cum_admissions_by_age',
-                            # 'cumTestsByPublishDate' : 'cum_tests',
-                            # 'newTestsByPublishDate' : 'new_tests',
-                            # 'covidOccupiedMVBeds' : 'covid_occupied_mv_beds',
-                            # 'hospitalCases' : 'hospital_cases',
-                            # 'plannedCapacityByPublishDate' : 'planned_capacity',
-                            # 'newDeaths28DaysByPublishDate' : 'new_deaths_28days_by_publish_date',
-                            # 'cumDeaths28DaysByPublishDate' : 'cum_deaths_28days_by_publish_date',
-                            # 'cumDeaths28DaysByPublishDateRate' : 'cum_deaths_28days_by_publish_date_rate',
-                            # 'newDeaths28DaysByDeathDate' : 'new_deaths_28days_by_death_date',
-                            # 'cumDeaths28DaysByDeathDate' : 'cum_deaths_28days_by_death_date',
-                            # 'cumDeaths28DaysByDeathDateRate' : 'cum_deaths_28days_by_death_date_rate'})
-        
+                            'areaName': 'area_name',
+                            'areaCode': 'area_code',
+                            'newCasesByPublishDate': 'new_cases',
+                            'cumCasesByPublishDate': 'cum_cases'})
+
         return(local_uk_df)
 
-    def load_data(local_uk_df):
+    def load_data(local_uk_df, db_engine):
 
         """Load UK local data into Postgres Database"""
-
-        connection_uri = os.environ['connection_uri']
-        db_engine = create_engine(connection_uri)
 
         local_uk_df.to_sql(
             name="local_uk",
@@ -447,22 +394,20 @@ def local_uk_data_etl():
 
     data = extract_data()
     data = transform_data(data)
-    load_data(data)
+    load_data(data, db_engine)
 
 
 @etl_decorator
-def owid_global_vaccinations_etl():
-    
+def owid_global_vaccinations_etl(db_engine):
+
     # Extract owid vaccinations data
     owid_global_vaccinations_url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
-    owid_global_vaccinations_df  = pd.read_csv(owid_global_vaccinations_url)
+    owid_global_vaccinations_df = pd.read_csv(owid_global_vaccinations_url)
 
-    owid_global_vaccinations_df.date = pd.to_datetime(owid_global_vaccinations_df.date)
+    owid_global_vaccinations_df.date = pd.to_datetime(
+                                            owid_global_vaccinations_df.date)
 
     # Load owid vaccinations data
-    connection_uri = os.environ['connection_uri']
-    db_engine = create_engine(connection_uri)
-
     owid_global_vaccinations_df.to_sql(
         name="owid_global_vaccinations",
         con=db_engine,
@@ -472,29 +417,24 @@ def owid_global_vaccinations_etl():
 
 
 @etl_decorator
-def bloomberg_global_vaccinations_etl():
-    
+def bloomberg_global_vaccinations_etl(db_engine):
+
     # Extract owid vaccinations data
     bloomberg_global_vaccinations_url = "https://raw.githubusercontent.com/BloombergGraphics/covid-vaccine-tracker-data/master/data/current-global.csv"
-    bloomberg_global_vaccinations_df  = pd.read_csv(bloomberg_global_vaccinations_url)
+    bloomberg_global_vaccinations_df = pd.read_csv(
+                                            bloomberg_global_vaccinations_url)
 
     # Load owid vaccinations data
-    connection_uri = os.environ['connection_uri']
-    db_engine = create_engine(connection_uri)
-
     bloomberg_global_vaccinations_df.to_sql(
         name="bloomberg_global_vaccinations",
         con=db_engine,
         if_exists="replace",
         index=False,
         method='multi')
-    
+
 
 @etl_decorator
-def create_data_files():
-
-    connection_uri = os.environ['connection_uri']
-    db_engine = create_engine(connection_uri)
+def create_data_files(db_engine):
 
     dirname = os.path.dirname(__file__)
 
@@ -519,21 +459,30 @@ def create_data_files():
                 index=False
                 )
 
-def etl():
-    jhu_cases_etl()
-    jhu_deaths_etl()
-    jhu_lookup_etl()
-    jhu_us_cases_etl()
-    jhu_us_deaths_etl()
-    us_states_etl()
-    local_uk_data_etl()
-    owid_global_vaccinations_etl()
-    bloomberg_global_vaccinations_etl()
-    create_data_files()
+
+def etl(db_engine):
+
+    jhu_cases_etl(db_engine)
+    jhu_deaths_etl(db_engine)
+    jhu_lookup_etl(db_engine)
+    jhu_us_cases_etl(db_engine)
+    jhu_us_deaths_etl(db_engine)
+    us_states_etl(db_engine)
+    local_uk_data_etl(db_engine)
+    owid_global_vaccinations_etl(db_engine)
+    bloomberg_global_vaccinations_etl(db_engine)
+    create_data_files(db_engine)
+
 
 if __name__ == '__main__':
-    start = time.perf_counter()
-    etl()
-    end = time.perf_counter()
-    print(f"\nLoaded data in {end-start:0.4f}s")
 
+    start = time.perf_counter()
+
+    connection_uri = os.environ['connection_uri']
+    db_engine = create_engine(connection_uri)
+
+    etl(db_engine)
+
+    end = time.perf_counter()
+
+    print(f"\nLoaded data in {end-start:0.4f}s")
